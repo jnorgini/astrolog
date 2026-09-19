1ad3916520b901999a3a694857ffe89fcb8ff43b
jnorgini <jnorgini@gmail.com>
2026-09-18 23:45:26 -0300

    chore: initial project skeleton

---
+++ backend/src/main/java/com/norgini/AstrologApplication.java
@@ -0,0 +1,13 @@
+package com.norgini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstrologApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstrologApplication.class, args);
+	}
+
+}
