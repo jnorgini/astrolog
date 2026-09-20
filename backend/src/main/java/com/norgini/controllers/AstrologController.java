b307b63d8d7c4f3f9a6b15c6558116586639c03e
jnorgini <jnorgini@gmail.com>
2026-09-20 19:13:05 -0300

    feat: hour minute parameters and cross origin annotation

--- backend/src/main/java/com/norgini/controllers/AstrologController.java
+++ backend/src/main/java/com/norgini/controllers/AstrologController.java
@@ -1,8 +1,8 @@
 package com.norgini.controllers;
 
 import java.util.List;
-
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,17 +16,21 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/astrolog")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class AstrologController {
 
 	private final AstrologService astrologyService;
 
 	@GetMapping("/planets")
-	public ResponseEntity<List<PlanetPositionResponse>> getAllPlanets(
-			@RequestParam int day, 
-			@RequestParam int month,
-			@RequestParam int year) {
+	public ResponseEntity<List<PlanetPositionResponse>> getAllPlanets(@RequestParam int day, @RequestParam int month,
+			@RequestParam int year, @RequestParam int hour, @RequestParam int minute) {
 
-		List<PlanetPositionResponse> result = astrologyService.getAllPlanets(day, month, year);
+		double defaultLatitude = -23.5333;
+		double defaultLongitude = -46.6167;
+
+		List<PlanetPositionResponse> result = astrologyService.getAllPlanets(day, month, year, hour, minute,
+				defaultLatitude, defaultLongitude);
+
 		return ResponseEntity.ok(result);
 	}
 }
