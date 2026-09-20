b264aa06e928c37bfad85a2c1dcba40d52d51132
jnorgini <jnorgini@gmail.com>
2026-09-20 19:11:34 -0300

    feat: hour and minute parameters in astrolog service

--- backend/src/main/java/com/norgini/services/AstrologService.java
+++ backend/src/main/java/com/norgini/services/AstrologService.java
@@ -1,25 +1,24 @@
 package com.norgini.services;
 
 import java.util.List;
-
 import org.springframework.stereotype.Service;
-
 import com.norgini.dtos.PlanetPositionResponse;
 import com.norgini.engines.AstrologEngine;
 import com.norgini.mappers.AstrologMapper;
-
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class AstrologService {
 
-	private final AstrologEngine astrologyEngine;
+	private final AstrologEngine astrologEngine;
 	private final AstrologMapper astrologMapper;
 
-	public List<PlanetPositionResponse> getAllPlanets(int day, int month, int year) {
-		var calculatedPositions = astrologyEngine.calculatePositions(day, month, year);
+	public List<PlanetPositionResponse> getAllPlanets(int day, int month, int year, int hour, int minute,
+			double latitude, double longitude) {
+		var calculatedPositions = astrologEngine.calculatePositions(day, month, year, hour, minute, latitude,
+				longitude);
+
 		return calculatedPositions.stream().map(astrologMapper::toResponse).toList();
 	}
-	
 }
