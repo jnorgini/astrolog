67b4923a5a22b146853e600a0901e82011c202aa
jnorgini <jnorgini@gmail.com>
2026-09-20 19:06:02 -0300

    refactor: astrolog engine architecture split and cleanup

--- backend/src/main/java/com/norgini/engines/AstrologEngine.java
+++ backend/src/main/java/com/norgini/engines/AstrologEngine.java
@@ -1,9 +1,10 @@
 package com.norgini.engines;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
-
 import org.springframework.stereotype.Component;
 
 import com.norgini.dtos.PlanetCalculationResult;
@@ -11,29 +12,40 @@ import com.norgini.enums.FixedPlanet;
 import com.norgini.enums.ZodiacSign;
 
 import swisseph.SweConst;
-import swisseph.SweDate;
 import swisseph.SwissEph;
 
 @Component
 public class AstrologEngine {
 
-	public List<PlanetCalculationResult> calculatePositions(int day, int month, int year) {
+	public List<PlanetCalculationResult> calculatePositions(int day, int month, int year, int hour, int minute,
+			double latitude, double longitude) {
 		SwissEph sw = new SwissEph();
 		try {
-			double julianDay = new SweDate(year, month, day, 12.0).getJulDay();
+			double targetTime = TimeConverter.targetTime(day, month, year, hour, minute);
 			StringBuffer errMsg = new StringBuffer();
+			double[] cusps = new double[13];
+			double[] ascmc = new double[10];
+			sw.swe_houses(targetTime, 0, latitude, longitude, (int) 'P', cusps, ascmc);
 
-			return Stream.of(FixedPlanet.values()).map(planet -> {
+			List<PlanetCalculationResult> finalResult = new ArrayList<>(Stream.of(FixedPlanet.values()).map(planet -> {
 				double[] xp = new double[6];
-				int ret = sw.swe_calc_ut(julianDay, planet.getId(), SweConst.SEFLG_SPEED, xp, errMsg);
-
-				if (ret >= 0) {
+				if (sw.swe_calc_ut(targetTime, planet.getId(), SweConst.SEFLG_SPEED, xp, errMsg) >= 0) {
 					int signIndex = (int) (xp[0] / 30) % 12;
-					String formattedDegrees = String.format("%.2f°", xp[0] % 30);
-					return new PlanetCalculationResult(planet, ZodiacSign.getByIndex(signIndex), formattedDegrees);
+					String degrees = String.format("%.2f°", xp[0] % 30);
+					int house = HouseDetector.findHouseForPlanet(xp[0], cusps);
+
+					return new PlanetCalculationResult(planet, ZodiacSign.getByIndex(signIndex), degrees, house,
+							xp[3] < 0, null);
 				}
 				return null;
-			}).filter(Objects::nonNull).toList();
+			}).filter(Objects::nonNull).toList());
+
+			IntStream.rangeClosed(1, 12).mapToObj(i -> AstrologFactory.createHouse(i, cusps[i]))
+					.forEach(finalResult::add);
+
+			finalResult.add(0, AstrologFactory.createAscendant(ascmc[0]));
+
+			return finalResult;
 		} finally {
 			sw.swe_close();
 		}
