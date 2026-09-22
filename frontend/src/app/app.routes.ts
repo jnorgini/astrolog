3b0470b24a18c95a5ea8c1a12ecbaea98d6040c7
jnorgini <jnorgini@gmail.com>
2026-09-22 19:51:07 -0300

    feat: about and contact links

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { AstrologHome } from './components/astrolog-home/astrolog-home';
+import { About } from './components/about/about';
 
 export const routes: Routes = [
-  { path: '', component: AstrologHome }, 
-  { path: '**', redirectTo: '' } 
+  { path: '', component: AstrologHome },
+  { path: 'about', component: About },
+  { path: '**', redirectTo: '' },
 ];
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,16 +1,14 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router'; 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive], 
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('astrolog-frontend');
-  
-  // Captura o ano atual dinamicamente para o Footer
   protected readonly currentYear = new Date().getFullYear();
 }
